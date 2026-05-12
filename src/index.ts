8ba6b365a5b370d86a8b884a0073704fd6b29f8a
Luciano Aedo Puga <81434301+shvmeless@users.noreply.github.com>
2026-05-11 20:33:04 -0400

    feat: add `work` raw search function

--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,4 @@ export { type RawArtistSearch, searchRawArtist } from '@requests/artist/raw-sear
 export { type RawReleaseGroupSearch, searchRawReleaseGroup } from '@requests/release-group/raw-search'
 export { type RawReleaseSearch, searchRawRelease } from '@requests/release/raw-search'
 export { type RawRecordingSearch, searchRawRecording } from '@requests/recording/raw-search'
+export { type RawWorkSearch, searchRawWork } from '@requests/work/raw-search'
