360cd7f646368b9f3ef9ca0112dee8e9fcd2cbf7
Luciano Aedo Puga <81434301+shvmeless@users.noreply.github.com>
2026-05-12 10:15:01 -0400

    feat: add `release` browse function

--- src/index.ts
+++ src/index.ts
@@ -48,3 +48,4 @@ export { lookupWork } from '@requests/work/basic-lookup'
 // BROWSE
 export { type ArtistBrowse, browseArtist } from '@requests/artist/basic-browse'
 export { type ReleaseGroupBrowse, browseReleaseGroup } from '@requests/release-group/basic-browse'
+export { type ReleaseBrowse, browseRelease } from '@requests/release/basic-browse'
