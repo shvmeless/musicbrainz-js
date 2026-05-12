e059ff82bf299c506b70ebdf828cf0b78fc76e0c
Luciano Aedo Puga <81434301+shvmeless@users.noreply.github.com>
2026-05-11 20:31:36 -0400

    feat: add `release-group` raw search function

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export type { Entity } from '@interfaces/common'
 
 // SEARCH
 export { type RawArtistSearch, searchRawArtist } from '@requests/artist/raw-search'
+export { type RawReleaseGroupSearch, searchRawReleaseGroup } from '@requests/release-group/raw-search'
