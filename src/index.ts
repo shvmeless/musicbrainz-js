7ccc3cbd718552a4217d674149b2a189b625533b
Luciano Aedo Puga <81434301+shvmeless@users.noreply.github.com>
2026-05-11 20:37:33 -0400

    feat: add `artist` raw lookup function

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 // TYPES
 export type { Entity } from '@interfaces/common'
 
-// SEARCH
+// RAW-SEARCH
 export { type RawArtistSearch, searchRawArtist } from '@requests/artist/raw-search'
 export { type RawReleaseGroupSearch, searchRawReleaseGroup } from '@requests/release-group/raw-search'
 export { type RawReleaseSearch, searchRawRelease } from '@requests/release/raw-search'
 export { type RawRecordingSearch, searchRawRecording } from '@requests/recording/raw-search'
 export { type RawWorkSearch, searchRawWork } from '@requests/work/raw-search'
+
+// RAW-LOOKUP
+export { lookupRawArtist } from '@requests/artist/raw-lookup'
